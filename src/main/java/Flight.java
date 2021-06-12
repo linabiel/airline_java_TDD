030708f6fc031e7dcedccff87382b187b4a99dec
linabiel <linuka.b@gmail.com>
2021-06-12 15:18:26 +0100

    can add passengers only if there remaining seats

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -22,4 +22,75 @@ public class Flight {
         this.departureTime = departureTime;
     }
 
+    public ArrayList<CabinCrewMember> getCabinCrewMember() {
+        return cabinCrewMember;
+    }
+
+    public void setCabinCrewMember(ArrayList<CabinCrewMember> cabinCrewMember) {
+        this.cabinCrewMember = cabinCrewMember;
+    }
+
+    public int getPassengers() {
+        return passengers.size();
+    }
+
+
+    public Pilot getPilot() {
+        return pilot;
+    }
+
+    public void setPilot(Pilot pilot) {
+        this.pilot = pilot;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void addPassenger(Passenger passengers, Plane plane) {
+        if (getAvailableSeats(plane) >= getPassengers()) {
+            this.passengers.add(passengers);
+        }
+    }
+    public int getAvailableSeats(Plane plane) {
+        int planesCapacity = plane.getPlaneType().getCapacity();
+            return planesCapacity - getPassengers();
+
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,24 +1,67 @@
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Test;
 
-import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
 
     private Flight flight;
     private Pilot pilot;
     private CabinCrewMember cabinCrewMember;
-    private Passenger passenger;
+    private Passenger passenger1;
+    private Passenger passenger2;
+    private Passenger passenger3;
+    private Plane boeing;
     private Plane chopper;
+    private Plane twoSeater;
 
     @Before
     public void before() {
         flight = new Flight(pilot, chopper, "FHG7659", "EDI","EDI", "14:00");
         cabinCrewMember = new CabinCrewMember("Ann", "Flight Attendant");
-        passenger = new Passenger("Lina", 2);
+        passenger1 = new Passenger("Tango", 2);
+        passenger2 = new Passenger("Cash", 1);
+        passenger3 = new Passenger("Bon Jovi", 3);
         pilot = new Pilot("Arnold Schwarzenegger", "Captain", "J1");
         chopper = new Plane(PlaneType.CHOPPER);
+        twoSeater = new Plane(PlaneType.LANCAIR235);
+        boeing = new Plane(PlaneType.BOEING747);
+    }
 
+    @Test
+    public void canGetFlightNumber() {
+        assertEquals("FHG7659", flight.getFlightNumber());
+    }
 
+    @Test
+    public void canAddPassengers() {
+        flight.addPassenger(passenger1, boeing);
+        flight.addPassenger(passenger2, boeing);
+        assertEquals(2, flight.getPassengers());
+    }
+
+    @Test
+    public void canGetNumberOfBookedSeats() {
+        flight.addPassenger(passenger1, chopper);
+        flight.addPassenger(passenger2, chopper);
+        int bookedSeats = flight.getPassengers();
+        assertEquals(2, bookedSeats);
+    }
+
+    @Test
+    public void canReturnNumberOfAvailableSeats() {
+        flight.addPassenger(passenger1, chopper);
+        flight.addPassenger(passenger2, chopper);
+        int availableSeats = flight.getAvailableSeats(chopper);
+        assertEquals(8, availableSeats);
+    }
+
+    @Test
+    public void cantBookPassengerIfNoRemainingSeatsLeft() {
+        flight.addPassenger(passenger1, twoSeater);
+        flight.addPassenger(passenger2, twoSeater);
+        flight.addPassenger(passenger3, twoSeater);
+        flight.setPlane(twoSeater);
+        assertEquals(2, flight.getPassengers());
     }
 }
